4845a7a48c88b5251955a92a05de02e4a05c2543
Oleksandr <sashapylypenko.pov@gmail.com>
2026-02-02 21:54:32 +0200

    fix: anim

--- public/animation.js
+++ public/animation.js
@@ -1,11 +1,11 @@
 const mainTitle = document.querySelector('.anime-type');
-const textToDisplay = 'в_Україні';
+const textToDisplay = 'в Україні';
 
 function animateTitle(element, text, delay = 100) {
   for (let i = 0; i < text.length; i++) {
     setTimeout(() => {
       console.log(text[i]);
-      element.innerText += i === 1 ? text[i] + ' ' : text[i];
+      element.innerHTML += text[i];
     }, delay * i);
   }
 }
