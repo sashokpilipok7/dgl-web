d3cc6649fb9bb984f7ae43e351b025633a856722
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-03-16 22:11:21 +0200

    feat: add new images and enhance student section functionality

--- public/main.js
+++ public/main.js
@@ -31,3 +31,19 @@ nav_items_internal.forEach((item) => {
     menu.classList.remove('show');
   });
 });
+
+const showMoreBtn = document.querySelector('.btn-more-student');
+const studentCards = document.querySelectorAll('.students-section .col-md-4 ');
+// const studentVisibleCards = document.querySelectorAll('.students-section .col-md-4:not(.d-none)');
+
+showMoreBtn.addEventListener('click', () => {
+  const hiddenCards = document.querySelectorAll('.students-section .col-md-4.d-none');
+  if (hiddenCards.length === 0) {
+    showMoreBtn.classList.add('d-none');
+    return;
+  }
+
+  studentCards.forEach((card) => {
+    card.classList.remove('d-none');
+  });
+});
