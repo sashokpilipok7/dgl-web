e88fe7c1a9adb4fe4dc33011f03f4572bfae3d81
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-03-17 00:09:36 +0200

    feat: implement crossfade image functionality and add new images for banners and student sections

--- public/animation.js
+++ public/animation.js
@@ -34,3 +34,52 @@ const animateOnScroll = (targets) => {
 };
 
 animateOnScroll(restTitles);
+
+function startCrossfade(layer1Id, layer2Id, images, interval = 4000) {
+  const layer1 = document.getElementById(layer1Id);
+  const layer2 = document.getElementById(layer2Id);
+  if (!layer1 || !layer2) return;
+
+  let index = 1;
+  let showFirst = true;
+
+  setInterval(() => {
+    index = (index + 1) % images.length;
+
+    if (showFirst) {
+      layer2.src = images[index];
+      layer2.style.opacity = 1;
+      layer1.style.opacity = 0;
+    } else {
+      layer1.src = images[index];
+      layer1.style.opacity = 1;
+      layer2.style.opacity = 0;
+    }
+
+    showFirst = !showFirst;
+  }, interval);
+}
+
+startCrossfade('hero-layer-1', 'hero-layer-2', [
+  'img/banner/one.JPG',
+  'img/hero.jpg',
+  'img/banner/two.JPG',
+  'img/banner/three.jpg',
+  'img/banner/five.JPG',
+  'img/banner/six.JPG',
+  'img/banner/seven.WEBP',
+]);
+
+startCrossfade(
+  'cta-layer-1',
+  'cta-layer-2',
+  [
+    'img/students/nadia.jpg',
+    'img/students/alisa.JPG',
+    'img/students/dima.jpg',
+    'img/students/alisa2.JPG',
+    'img/students/julia.jpg',
+    'img/students/danic.jpg',
+  ],
+  5000
+);
